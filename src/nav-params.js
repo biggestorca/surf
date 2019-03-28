8f6785c588f5dceaecb36fa12a8fa993898f0235
biggestorca <imma.big.orca@gmail.com>
2019-03-28 15:34:39 +0200

    base frist screen styles, header, logo, nav

---
+++ src/nav-params.js
@@ -0,0 +1,6 @@
+module.exports = {
+  surf: require('../components/nav-surf.html'),
+  travel: require('../components/nav-travel.html'),
+  sleep: require('../components/nav-sleep.html'),
+  shop: require('../components/nav-shop.html'),
+};
