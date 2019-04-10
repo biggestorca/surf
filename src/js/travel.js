979324f9679b3a5d7d1afbd4fa8e373d944f3a66
biggestorca <imma.big.orca@gmail.com>
2019-04-10 17:47:18 +0300

    third slider animations

--- src/js/travel.js
+++ src/js/travel.js
@@ -1,6 +1,5 @@
 import Slider from './Slider';
-// import isNaN from './isNaN';
-// import { isIE } from './checkBrowser';
+import isNaN from './isNaN';
 import payload from './travelData';
 
 class SurfSlider extends Slider {
@@ -22,42 +21,77 @@ class SurfSlider extends Slider {
 
   setCurrentAirlineDestination() {
     const currentAirlineData = this.payload[this.activeItem];
+
     const currentLocation = this.mainElement.querySelector('#travel-location');
+    if (!isNaN(this.prevActiveItem)) {
+      currentLocation.classList.remove('animation__appear--on');
+    }
+
     currentLocation.innerHTML = `${currentAirlineData.name} <span class="divider">|</span> ${
       currentAirlineData.country
     }`;
+
+    setTimeout(() => {
+      currentLocation.classList.add('animation__appear--on');
+    }, 500);
   }
   setCurrentAirlineName() {
     const currentAirlineData = this.payload[this.activeItem];
     const $airlineName = this.mainElement.querySelector('#travel__airline-name');
+    if (!isNaN(this.prevActiveItem)) {
+      $airlineName.classList.remove('animation__appear--on');
+    }
     $airlineName.innerText = currentAirlineData.airline;
+    setTimeout(() => {
+      $airlineName.classList.add('animation__appear--on');
+    }, 500);
   }
   setCurrentAirlineLocationImage() {
     const currentAirlineData = this.payload[this.activeItem];
     const $elem = this.mainElement.querySelector('#travel__location-image');
+    if (!isNaN(this.prevActiveItem)) {
+      $elem.classList.remove('animation__appear--on');
+    }
     $elem.setAttribute('src', currentAirlineData.airlineLocationImage);
+
+    setTimeout(() => {
+      $elem.classList.add('animation__appear--on');
+    }, 500);
   }
   setCurrentAirlineParameters() {
     const currentAirlineData = this.payload[this.activeItem];
 
     const $destination = this.mainElement.querySelector('#travel__destination');
-    $destination.innerHTML = `${currentAirlineData.city} <br/> ${currentAirlineData.country}`;
-
     const $distance = this.mainElement.querySelector('#travel__distance');
-    $distance.innerText = `${currentAirlineData.distance} miles`;
-
     const $time = this.mainElement.querySelector('#travel__time');
-    $time.innerHTML = `${(currentAirlineData.timeMinutes / 60).toFixed(0)} hours <br/> ${currentAirlineData.timeMinutes % 60} minutes`;
-
     const $pricing = this.mainElement.querySelector('#travel__pricing');
-    $pricing.innerText = `$ ${currentAirlineData.price} usd`;
-
     const $round = this.mainElement.querySelector('#round-trip');
+
+    if (!isNaN(this.prevActiveItem)) {
+      $destination.classList.remove('animation__appear--on');
+      $distance.classList.remove('animation__appear--on');
+      $time.classList.remove('animation__appear--on');
+      $pricing.classList.remove('animation__appear--on');
+      $round.classList.remove('animation__fade-in-right--on');
+    }
+
+    $destination.innerHTML = `${currentAirlineData.city} <br/> ${currentAirlineData.country}`;
+    $distance.innerText = `${currentAirlineData.distance} miles`;
+    $time.innerHTML = `${(currentAirlineData.timeMinutes / 60).toFixed(0)} hours <br/> ${currentAirlineData.timeMinutes % 60} minutes`;
+    $pricing.innerText = `$ ${currentAirlineData.price} usd`;
     if (currentAirlineData.roundTrip) {
       $round.innerText = 'round trip';
     } else {
       $round.innerText = '';
     }
+
+    setTimeout(() => {
+      $destination.classList.add('animation__appear--on');
+      $distance.classList.add('animation__appear--on');
+      $time.classList.add('animation__appear--on');
+      $pricing.classList.add('animation__appear--on');
+      $round.classList.add('animation__fade-in-right--on');
+    }, 250);
   }
 }
 
