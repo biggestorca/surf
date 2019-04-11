4e908aae416e486a7ed64f709e0df496fcea31a9
biggestorca <imma.big.orca@gmail.com>
2019-04-11 18:04:31 +0300

    lazy loading for images, add x2 images, add low quality images, improve lazy loading for IE11

--- src/js/lazyImages.js
+++ src/js/lazyImages.js
@@ -1,3 +1,5 @@
+const Io = require('intersection-observer');
+
 const lazyImagesLoader = () =>
   document.addEventListener('DOMContentLoaded', () => {
     const lazyImages = Array.prototype.slice.call(document.querySelectorAll('img.lazy'));
@@ -24,7 +26,26 @@ const lazyImagesLoader = () =>
         lazyImageObserver.observe(lazyImage);
       });
     } else {
-      // Possibly fall back to a more compatible method here
+      const lazyImageObserver = new Io((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const lazyImage = entry.target;
+            if (lazyImage.dataset.src) {
+              lazyImage.src = lazyImage.dataset.src;
+            }
+            if (lazyImage.dataset.srcset) {
+              lazyImage.srcset = lazyImage.dataset.srcset;
+            }
+
+            lazyImage.classList.remove('lazy');
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+
+      lazyImages.forEach((lazyImage) => {
+        lazyImageObserver.observe(lazyImage);
+      });
     }
   });
 
